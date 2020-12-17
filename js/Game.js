f7c601b5944bae04b7272238bbecb62d297c44be
PriyalShrimali <68710858+PriyalShrimali@users.noreply.github.com>
2020-12-17 23:12:19 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,69 @@
+class Game {
+    constructor(){
+
+    }
+    getState(){
+        var gameStateRef  = database.ref('gameState');
+        gameStateRef.on("value",function(data){
+           gameState = data.val();
+        })
+    
+      }
+    
+      update(state){
+        database.ref('/').update({
+          gameState: state
+        });
+      }
+      
+  async start(){
+    if(gameState === 0){
+      console.log("HELLO");
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+      playerA = createSprite(350,450,50,50);
+      playerA.addImage("p1",outsiders);
+      playerB = createSprite(650,600,50,50);
+      playerB.addImage("p2",outsiders);
+      playerC = createSprite(950,450,50,50);
+      playerC.addImage("p3",outsiders);
+      playerD = createSprite(475,250,50,50);
+      playerD.addImage("p4",outsiders);
+      playerE = createSprite(825,250,50,50);
+      playerE.addImage("p5",outsiders);
+      playerF = createSprite(600,415,50,50);
+      playerF.addImage("p6",insiders);
+      playerG = createSprite(700,415,50,50);
+      playerG.addImage("p7",insiders);
+      array = [playerA,playerB,playerC,playerD,playerE,playerF,playerG];
+    }
+}
+play(){
+  form.hide();
+
+  Player.getPlayerInfo();
+
+  if(allPlayers!==undefined){
+    background(backgroundImg);
+    var index = 0;
+    var x;
+    var y;
+    for(var plr in allPlayers){
+      index = index+1;
+      x = allPlayers[plr].x;
+      y = allPlayers[plr].y;
+
+      array[index -1].x = x;
+      array[index -1].y = y;
+    }
+  }
+  
+   drawSprites();
+}
+}
---
+++ sketch.js
@@ -0,0 +1,40 @@
+var backgroundImg;
+var ball;
+var insiders;
+var outsiders;
+var playerA,playerB,playerC,playerD,playerE,playerF,playerG;
+var gameState=0;
+var playerCount;
+var game;
+var database;
+var player;
+var playerCount;
+var form;
+var allPlayers;
+var array;
+
+function preload(){
+backgroundImg = loadImage("/images/backGround.png");
+ball = loadImage("/images/ball.png");
+insiders = loadImage("/images/insiders.png");
+outsiders = loadImage("/images/outsiders.png");  
+}
+function setup(){
+    canvas = createCanvas(displayWidth - 20, displayHeight-30);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+    console.log("testing");
+  }
+  function draw(){
+    background(backgroundImg);
+    if(playerCount===7){
+      game.update(1);
+    }
+    if(gameState===1){
+      clear();
+      game.play();
+    }
+  }
+  
